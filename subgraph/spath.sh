6809cf5d818c2d7a98defa67c25a13a47a3faa62
Abhishek Kumar <mailme.abhibits@gmail.com>
2020-07-19 16:47:27 +0530

    Add files via upload

---
+++ subgraph/spath.sh
@@ -0,0 +1,2 @@
+#! /bin/bash/
+python pathfinder.py --data_dir='./../datasets/ob_data/train/ob_concepts_train_final.mcp' --output_dir='./../datasets/ob_data/path/train/' --start=0 --end=500 --typ='train'
